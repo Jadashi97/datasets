16cc8f166f6d538a00e25f2e5bb702a1eed3f8a3
jadashi97 <njadauniversal23@yahoo.com>
2022-02-21 16:31:34 -0600

    added stuff

---
+++ Untitled1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pandas as pd
+
+df = pd.read_csv("Desktop/Data Science/AllCountries.csv")
+
+
+
+# In[7]:
+
+
+df
+
+
+# In[8]:
+
+
+df.head()
+
+
+# In[9]:
+
+
+df.tail()
+
+
+# In[11]:
+
+
+df.plot()
+
+
+# In[ ]:
+
+
+
+
